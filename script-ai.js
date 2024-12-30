bb58f2ace1dd0b414f3fb6ce17a995d86464c432
Arnav Mishra <acromatic121@gmail.com>
2024-12-30 12:33:22 +0530

    Create script-ai.js

---
+++ script-ai.js
@@ -0,0 +1,7 @@
+// Get the button element
+const button = document.querySelector('button');
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+    console.log('Button clicked!');
+});
